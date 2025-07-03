f46274164f860a03f3b48f93d1454933950aa27f
elikkatzgit <50174655+elikkatzgit@users.noreply.github.com>
2025-07-03 23:40:25 +0300

    Create iam.tf

---
+++ ft_code/iam.tf
@@ -0,0 +1,26 @@
+resource "aws_iam_role" "ec2_role" {
+  name = "EC2_ECR_EC2_SQS_Role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_policy" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.ecr_ec2_sqs_admin.arn
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "EC2InstanceProfile"
+  role = aws_iam_role.ec2_role.name
+}
