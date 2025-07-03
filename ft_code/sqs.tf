5180d94f51c4bd057cd148d6a16c20b766b2c5af
elikkatzgit <50174655+elikkatzgit@users.noreply.github.com>
2025-07-03 23:53:43 +0300

    Update sqs.tf

--- ft_code/sqs.tf
+++ ft_code/sqs.tf
@@ -1,14 +1,14 @@
-resource "aws_sqs_queue" "microservice1" {
-  name                      = "microservice1"
+resource "aws_sqs_queue" "sqs" {
+  name                      = "sqs"
   visibility_timeout_seconds = 30
   message_retention_seconds  = 345600
   delay_seconds              = 0
 }
 
 output "sqs_queue_url" {
-  value = aws_sqs_queue.microservice1.id
+  value = aws_sqs_queue.sqs.id
 }
 
 output "sqs_queue_arn" {
-  value = aws_sqs_queue.microservice1.arn
+  value = aws_sqs_queue.sqs.arn
 }
